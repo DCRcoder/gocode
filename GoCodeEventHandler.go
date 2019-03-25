88bc1d7d7d08ff9ffe1768cc2911a92ad265f2df
liubin <liubin01@zhihu.com>
2019-03-26 00:56:50 +0800

    feature: add ignore

--- GoCodeEventHandler.go
+++ GoCodeEventHandler.go
@@ -1,40 +1,76 @@
 package gocode
 
 import (
+	"io/ioutil"
 	"log"
 
 	"github.com/fsnotify/fsnotify"
-
 )
+
+var FilterName = []string{".git", "eggs", "develop-eggs", "gen-go", "gen-py"}
+
+func filterDir(fileName string) bool {
+	for _, name := range FilterName {
+		if name == fileName {
+			return true
+		}
+	}
+	return false
+}
+
+func findAllDir(path string) []string {
+	dirs := make([]string, 0, 0)
+	files, _ := ioutil.ReadDir(path)
+	for _, f := range files {
+		if filterDir(f.Name()) {
+			continue
+		}
+		subDirName := path + "/" + f.Name()
+		if f.IsDir() {
+			dirs = append(dirs, subDirName)
+			subDirs := findAllDir(subDirName)
+			dirs = append(dirs, subDirs...)
+		}
+	}
+	return dirs
+}
 
 func WatchAnyEvent(repo Repository) {
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer watcher.Close()
 
-	eventChan := make(chan bool)
-
+	done := make(chan bool)
 	go func() {
 		for {
 			select {
-			case event := <-watcher.Events:
-				log.Println("event: ", event)
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				log.Println("event:", event)
 				if event.Op&fsnotify.Write == fsnotify.Write {
-					log.Println("modified file: ", event.Name)
+					log.Println("modified file:", event.Name)
 				}
 				repo.Sync()
-			case err := <-watcher.Errors:
-				log.Println("error: ", err)
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
 			}
 		}
 	}()
-
+	dirs := findAllDir(repo.path)
 	err = watcher.Add(repo.path)
+	for _, dir := range dirs {
+		watcher.Add(dir)
+	}
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	<-eventChan
+	<-done
 }
--- repository.go
+++ repository.go
@@ -1,8 +1,12 @@
 package gocode
 
 import (
+	"bufio"
+	"fmt"
 	"log"
+	"os"
 	"os/exec"
+	"strings"
 	"time"
 )
 
@@ -10,11 +14,11 @@ type Repository struct {
 	path       string
 	remotePath string
 	lastSync   int64
-	ignoreRule string
 }
 
 func (repo Repository) Sync() {
 	now := time.Now().Unix()
+	log.Printf("Sync time: %d", now)
 	if repo.lastSync == 0 {
 		repo.lastSync = now
 		repo.doSync()
@@ -28,11 +32,43 @@ func (repo Repository) Sync() {
 
 func (repo Repository) doSync() {
 	log.Println("do syncing")
-	cmd := exec.Command("rsync", "-av", "--delete", "--exclude=.git/", repo.path, repo.remotePath)
+	ignoreContext := repo.ignore()
+	args := []string{"-avz", "--exclude", ".git"}
+
+	for _, c := range ignoreContext {
+		args = append(args, "--exclude", c)
+	}
+	args = append(args, repo.path, repo.remotePath)
+
+	cmd := exec.Command("rsync", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	fmt.Println(cmd)
 	err := cmd.Run()
 	if err != nil {
 		log.Println(err)
 	}
+	log.Println("sync finished")
+}
+
+func (repo Repository) ignore() []string {
+	f, e := os.Open(repo.path + "/.gitignore")
+	defer f.Close()
+	if e != nil {
+		log.Println("can`t find .gitignore file")
+	}
+
+	var excludeContext []string
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		liner := string(scanner.Bytes())
+		if strings.HasPrefix(string(liner), "#") || liner == "" {
+			continue
+		}
+		excludeContext = append(excludeContext, liner)
+	}
+	return excludeContext
 }
 
 func New(path string, remotePath string) Repository {
